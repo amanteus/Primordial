9bbfad4f48467752f914ce05b6ff127b351b49c1
amanteus <amanteusbr@gmail.com>
2025-06-19 17:01:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def sales_page():
-    # Você pode passar variáveis do Python para o HTML aqui se precisar no futuro
     return render_template('index.html')
+
+# ADICIONE ESTA NOVA ROTA
+@app.route('style.css')
+def serve_css():
+    return render_template('style.css'), 200, {'Content-Type': 'text/css'}
 
 if __name__ == '__main__':
     app.run(debug=True)
